21b4df691c1805ce4ba9f6ecddc7d91110d2d083
xLimbo <crjesus92x@gmail.com>
2019-04-15 19:39:20 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-wkuzca
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-wkuzca)
---
+++ index.js
@@ -0,0 +1,10 @@
+var edad = 40
+console.log("edad del usuario", edad)
+
+var edad = 90
+console.log("nueva edad", edad)
+
+var estatura = 1.98
+console.log("la estatura", estatura, "metros")
+
+
