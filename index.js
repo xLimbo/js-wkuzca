c9c6fb1ff2b259804bafe135dec9398757747d5a
xLimbo <crjesus92x@gmail.com>
2019-04-15 20:16:45 -0500

    1 file modified

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
-var edad = 40
-console.log("edad del usuario", edad)
+const persona = {
+  altura: 180,
+  edad: 40,
+  nombre: "Juan",
+ "nombre completo": "Juan Navaja"
+}
 
-var edad = 90
-console.log("nueva edad", edad)
+console.log("Datos de la persona", persona)
 
-var estatura = 1.98
-console.log("la estatura", estatura, "metros")
+console.log("Nombre = ", persona["nombre"] )
+
+console.log("Edad = ", persona.edad )
+
+console.log("Nombre completo = ",persona["nombre completo"])
+
+console.log("Nombre completo = ",persona.nombreCompleto)
+
+persona.peso = 80
+
+console.log("Con nuevo dato", persona)
+
+persona.experiencialaboral = {
+trabajo: "BCP",
+tiempo: "6 meses",
+
+cargo: "Analista funcional"
+
+}
+
+console.log("con experiencia", persona)
+
+
+console.log("Cargo = ", persona.experiencialaboral.cargo)
+
+console.log("otra forma 01 = ",persona["experiencialaboral"].cargo)
+
+console.log("otra forma 02 = ",persona["experiencialaboral"]["cargo"])
+
+console.log("otra forma 03 = ",persona.experiencialaboral["cargo"])
+
+delete persona.peso
+
+console.log("Nueva persona = ", persona)
+
 
 
